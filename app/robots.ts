64e6f4562fda0078fac85d09cf7785142879b854
Dipa khanal <khanaldeepa126@gmail.com>
2026-05-12 16:03:50 +0545

    build changes

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 const SITE_URL = "https://www.khanaldipa.com.np";
 
 export default function robots(): MetadataRoute.Robots {
